cd7359047017790efb97eeeff2fabbccb85b1cbb
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-06 15:56:47 +0900

    Update #10871.py

--- 단계별로 풀어보기/3단계/#10871.py
+++ 단계별로 풀어보기/3단계/#10871.py
@@ -1,6 +1,11 @@
+#10871 X보다 작은 수
+
 n, x = map(int, input().split())
 
-for i in range(n):
-    a = list()
-    input = map(int, input.split())
-    a.append(input)
+input_list = list(map(int, input().split()))
+print_list = []
+for number in input_list:
+    if number < x:
+        print_list.append(number)
+for i in print_list:
+    print(i,end = ' ')
