dc0d0a7519932d6b9241d8d986d4c5b5958448b2
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-15 00:19:03 +0900

    Update #1110.py

--- 단계별로 풀어보기/4단계/#1110.py
+++ 단계별로 풀어보기/4단계/#1110.py
@@ -1,16 +1,18 @@
 #1101 더하기 사이클
 
 n = input()
-count = 1
-new = {}
+count = 0
+new_num = []
 if len(n) == 1:
     n = "0" + n
-new[count] = str(int(n[0]) + int(n[1]))
-count += 1
-print(new)
-print(new[count-1][0])
-while (n!=new[count-1]):
-    new[count] = int(new[count-1][0]) + int(new[count-1][0])
+new_num.append(str(int(n[0]) + int(n[1])))
+
+while n != new_num[-1]:
+    if len(new_num[-1]) == 1:
+        new_num[-1] = "0" + new_num[-1]
+    if count == 0:
+        new_num.append(str(int(n[1]) + int(new_num[-1][1])))
+    else:
+        new_num.append(str(int(new_num[-2][1]) + int(new_num[-1][1])))
     count += 1
-    print(new)
 print(count)
