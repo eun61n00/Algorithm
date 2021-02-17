8aa65513fa4d124c1e59e0bc62d3f36d4dbdba6b
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-18 00:23:48 +0900

    Update #1546.py

--- 단계별로 풀어보기/5단계/#1546.py
+++ 단계별로 풀어보기/5단계/#1546.py
@@ -1,1 +1,9 @@
+#1546 평균
 
+n = int(input())
+new_score = []
+original_score = list(map(int, input().split()))
+max = max(original_score)
+for i in range(n):
+    new_score.append(original_score[i]/max*100)
+print(sum(new_score)/n)
