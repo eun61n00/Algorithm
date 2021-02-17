1b9e673cb4754d5b45068cd013ebd2794005becb
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-17 23:41:04 +0900

    Create #2577.py

---
+++ 단계별로 풀어보기/5단계/#2577.py
@@ -0,0 +1,18 @@
+#2577 숫자의 개수
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+n = str(a * b * c)
+
+print(n.count('0'))
+print(n.count('1'))
+print(n.count('2'))
+print(n.count('3'))
+print(n.count('4'))
+print(n.count('5'))
+print(n.count('6'))
+print(n.count('7'))
+print(n.count('8'))
+print(n.count('9'))
