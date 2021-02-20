45e72accf35473cbd75eb75474aae8a895cba271
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-20 23:55:21 +0900

    Update #1965.py

--- 단계별로 풀어보기/6단계/#1965.py
+++ 단계별로 풀어보기/6단계/#1965.py
@@ -1,1 +1,19 @@
-
+def hansu(x):
+    x = str(x)
+    count = 1
+    list = []
+    result = True
+    
+    while count != len(x):
+        for i in range(len(x)):
+            difference = int(x[i])-int(x[i+1])
+            list.append(difference)
+            count += 1
+    print(list)    
+    for i in range(len(list)):
+        if list[i] == list[i+1]:
+            result = True
+        else:
+            result = False
+            
+    return result
