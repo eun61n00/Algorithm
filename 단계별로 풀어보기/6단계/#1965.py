1c8bd31218f5adf2849dfa230db6ce8f2e100ed0
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-22 11:46:56 +0900

    Update #1965.py

--- 단계별로 풀어보기/6단계/#1965.py
+++ 단계별로 풀어보기/6단계/#1965.py
@@ -1,19 +1,24 @@
 def hansu(x):
     x = str(x)
-    count = 1
+    count = 0
     list = []
     result = True
     
-    while count != len(x):
-        for i in range(len(x)):
+    while count != len(x)-1:
             difference = int(x[i])-int(x[i+1])
             list.append(difference)
             count += 1
-    print(list)    
-    for i in range(len(list)):
-        if list[i] == list[i+1]:
-            result = True
-        else:
-            result = False
+    setlist = set(list)
+    if len(setlist) == 0 or len(setlist) == 1:
+        return True
+    else:
+        return False
             
     return result
+
+n = int(input())
+TFlist = []
+for i in range(n):
+    TFlist.append(hansu(i+1))
+ans = TFlist.count(True)
+print(ans)
