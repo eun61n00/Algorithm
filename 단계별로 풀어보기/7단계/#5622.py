157127cd33b4811cef3801b0fc78ef0e8dff50c5
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-24 15:14:57 +0900

    Update #5622.py

--- 단계별로 풀어보기/7단계/#5622.py
+++ 단계별로 풀어보기/7단계/#5622.py
@@ -1,1 +1,24 @@
-
+alphabet = input()
+list = []
+time = []
+for i in alphabet:
+    list.append(ord(i))
+for i in range(len(list)):
+    if list[i] <= 67:
+        time.append(3)
+    elif list[i] <= 70:
+        time.append(4)
+    elif list[i] <=73:
+        time.append(5)
+    elif list[i] <= 76:
+        time.append(6)
+    elif list[i] <= 79:
+        time.append(7)
+    elif list[i] <= 83:
+        time.append(8)
+    elif list[i] <= 86:
+        time.append(9)
+    else:
+        time.append(10)
+result = sum(time)
+print(result)
