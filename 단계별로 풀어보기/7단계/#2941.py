7855a7ec58f4fbea1cf5b0a550cfc44cf4b3c88c
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-24 17:39:05 +0900

    Update #2941.py

--- 단계별로 풀어보기/7단계/#2941.py
+++ 단계별로 풀어보기/7단계/#2941.py
@@ -1,1 +1,30 @@
-
+s = input()
+count = 0
+while (s.find("c=") != -1):
+    count += 1
+    s = s.replace("c=", " ", 1)
+while (s.find("c-") != -1):
+    count += 1
+    s = s.replace("c-", " ", 1)
+while (s.find("dz=") != -1):
+    count += 1
+    s = s.replace("dz=", " ", 1)
+while (s.find("d-") != -1):
+    count += 1
+    s = s.replace("d-", " ", 1)
+while (s.find("lj") != -1):
+    count += 1
+    s = s.replace("lj", " ", 1)
+while (s.find("nj") != -1):
+    count += 1
+    s = s.replace("nj", " ", 1)
+while (s.find("s=") != -1):
+    count += 1
+    s = s.replace("s=", " ", 1)
+while (s.find("z=") != -1):
+    count += 1
+    s = s.replace("z=", " ", 1)
+s=s.replace(" ", '')
+for i in s:
+    count += 1
+print(count)
