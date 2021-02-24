0e1c0bb7a16cde0cf7dd04882cd9053b3c3653fd
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-02-24 12:19:41 +0900

    Update #1157.py

--- 단계별로 풀어보기/7단계/#1157.py
+++ 단계별로 풀어보기/7단계/#1157.py
@@ -1,1 +1,11 @@
-
+import collections
+word = input()
+word = word.upper()
+c = collections.Counter(word)
+answer = c.most_common()
+if len(answer) == 1:
+    print(answer[0][0])
+elif answer[0][1] == answer[1][1]:
+    print ('?')
+else:
+    print(answer[0][0])
