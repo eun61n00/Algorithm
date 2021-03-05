7449436c8be977539c69fec846e10a3e3c1e9baf
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-03-05 17:08:18 +0900

    Update #8958.py

--- 단계별로 풀어보기/5단계/#8958.py
+++ 단계별로 풀어보기/5단계/#8958.py
@@ -1,1 +1,15 @@
-
+n = int(input())
+score = 0
+result = 0
+for i in range(n):
+    OXlist = input()
+    for j in range(len(OXlist)):
+        if OXlist[j] == "O":
+            score += 1
+            result += score
+        else:
+            score = 0
+            continue
+    print(result)
+    score=0
+    result=0
