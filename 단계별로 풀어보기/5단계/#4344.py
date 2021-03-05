3dfee84d6c9d527c6cacdabd67c73d991108d13a
eun61n00 <71613548+eun61n00@users.noreply.github.com>
2021-03-05 17:36:21 +0900

    Update #4344.py

--- 단계별로 풀어보기/5단계/#4344.py
+++ 단계별로 풀어보기/5단계/#4344.py
@@ -1,1 +1,18 @@
+c = int(input())
+sum = 0
+avg = 0
+result = 0
 
+for i in range(c):
+    num_list = list(map(int, input().split()))
+    student = num_list[0]
+    del num_list[0]
+    for j in range(student):
+        sum += num_list[j]
+        avg = sum/student
+        isBiggerThanAvg = [x for x in num_list if x>avg]
+        result = (len(isBiggerThanAvg)/len(num_list))*100
+    print(format(result, ".3f"),"%", sep='')
+    sum = 0
+    avg = 0
+    result = 0
