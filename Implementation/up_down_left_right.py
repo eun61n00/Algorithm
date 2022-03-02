5b9345e4a728109a75b3bb9dcbe9ae3ab933e387
eun61n00 <eunbin0624@seoultech.ac.kr>
2022-03-02 23:44:33 +0900

    implement - 상하좌우문제

--- Implementation/up_down_left_right.py
+++ Implementation/up_down_left_right.py
@@ -1,25 +1,22 @@
-# coding=utf-8
 # <이것이 코딩 테스트다>(나동빈, 한빛미디어) 예제 4-1
 from sys import stdin
 
 n = int(stdin.readline())
-plans = ['R', 'R', 'R', 'U', 'D', 'D']
-# plan = list(map(int, input().split()))
-loc_x = 1
-loc_y = 1
+plans = input().split()
+x, y = 1, 1
 dir_x = [0, 0, -1, 1]
 dir_y = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
 
 for plan in plans:
-    if plan == 'L':
-        loc_x += dir_x[0]
-        loc_y += dir_y[0]
-    elif plan == 'R':
-        loc_x += dir_x[1]
-        loc_y += dir_y[1]
-    elif plan == 'U':
-        loc_x += dir_x[2]
-        loc_y += dir_y[2]
-    else:
-        loc_x += dir_x[3]
-        loc_y += dir_y[3]
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dir_x[i]
+            ny = y + dir_y[i]
+
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+
+    x, y = nx, ny
+
+print(x, y)
