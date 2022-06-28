4ab2a9a3867f40c9a48820a0ddb6a6dc0e763fa4
eun61n00 <eunbin0624@seoultech.ac.kr>
2022-06-28 15:10:04 +0900

    add utf-8 header

--- Dynamic Programming/boj_1904.py
+++ Dynamic Programming/boj_1904.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
 # boj 1904 01타일
 
 import sys
 input = sys.stdin.readline
+
 
 def bin_tile(n):
 	dp = [0] * 1000001
 	dp[1] = 1
 	dp[2] = 2
 	for i in range(3, n + 1):
-		dp[i] = (dp[i - 2] + dp[i - 1])%15746
+		dp[i] = (dp[i - 2] + dp[i - 1])# %15746
 	return dp[n]
 
 if __name__ == '__main__':
