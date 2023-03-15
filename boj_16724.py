28e5ca627492c6c131427b8d696ed88c2a41ce21
eunbin <eun61n@gmail.com>
2023-03-15 23:00:01 +0900

    [SOLVED] 230316

--- boj_16724.py
+++ boj_16724.py
@@ -2,84 +2,41 @@
 # -*- coding: utf-8 -*-
 # boj 16724 피리 부는 사나이
 
-
+import sys
 from collections import deque
 
+input = sys.stdin.readline
 n, m = map(int, input().split())
 
 board = []
 for _ in range(n):
-    board.append(list(map(str, input())))
+    board.append(list(map(str, input().rstrip())))
 
 # 방향 설정
-movements = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+movements = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+visited = [[False] * m for _ in range(n)]
 
-parent = [i for i in range(n * m)]
+def dfs(board, x, y, visited):
+    global idx
+    if x < 0 or y < 0 or x > n - 1 or y > m - 1:
+        return 0
 
-def find(x):
-    global parent
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-        return parent[x]
-    return x
+    if visited[x][y] != False:
+        if visited[x][y] == idx:
+            safe_zone.append((x, y))
+        return 1
 
-def union(x, y):
-    global parent
-    visited[x], visited[y] = True, True
-    x = find(x)
-    y = find(y)
-    if x == y:
-        return
-    if x > y:
-        parent[x] = y
-        parent = [y if i == x else i for i in parent]
-    else:
-        parent[y] = x
-        parent = [x if i == y else i for i in parent]
+    visited[x][y] = idx
+    dx, dy = movements[board[x][y]]
+    nx, ny = x + dx, y + dy
+    dfs(board, nx, ny, visited)
 
-queue = deque()
-visited = [False for _ in range(n * m)]
+safe_zone = []
+idx = 1
+for i in range(n):
+    for j in range(m):
+        if visited[i][j] == False:
+            dfs(board, i, j, visited)
+            idx += 1
 
-while False in visited:
-    idx = [idx for idx, t in enumerate(visited) if t == False][0]
-    queue.append((idx // m, idx % m))
-    while queue:
-        x, y = queue.popleft()
-        cur = x * m + y
-        if board[x][y] == 'U':
-            dx, dy = movements[0]
-            n = -m
-        elif board[x][y] == 'D':
-            dx, dy = movements[1]
-            n = +m
-        elif board[x][y] == 'L':
-            dx, dy = movements[2]
-            n = -1
-        else:
-            dx, dy = movements[3]
-            n = +1
-
-        nx, ny = x + dx, y + dy
-
-        if visited[nx * m + ny] == False:
-            queue.append((nx, ny))
-        visited[x * m + y] = True
-        visited[nx * m + ny] = True
-        union(cur, cur + n)
-
-
-# for i in range(n):
-#     for j in range(m):
-#         cur = i * m + j
-#         # if visited[cur] == True:
-#             # continue
-#         if board[i][j] == 'U':
-#             union(cur, cur - m)
-#         elif board[i][j] == 'D':
-#             union(cur, cur + m)
-#         elif board[i][j] == 'R':
-#             union(cur, cur + 1)
-#         else:
-#             union(cur, cur - 1)
-
-print(len(set(parent)))
+print(len(safe_zone))
