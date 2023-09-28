cf15f4a1b1dcab1472ad44e8ed28617b64eff5b7
eun61n00 <eun61n@gmail.com>
2023-09-28 20:10:14 +0900

    Edit: 소수 찾기 (programmers)

--- Bruteforcing/programmers_소수찾기.py
+++ Bruteforcing/programmers_소수찾기.py
@@ -1,19 +1,40 @@
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 프로그래머스 소수 찾기
 
-def is_prime(num):
-	if num <= 2:
-		return False
+from itertools import permutations
+from collections import defaultdict
+import math
+
+check = defaultdict(bool)
+
+
+def is_prime(x):
+    if x == 0 or x == 1:
+        return 0
+
+    if check[x] == True:
+        return 0
+
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return 0
+
+    check[x] = True
+    return 1
 
 
 def solution(numbers):
-	answer = 0
-	num_list = [n for n in numbers]
+    answer = 0
+    num_list = [ch for ch in numbers]
 
-	for i in range(len(num_list)):
+    for n in num_list:
+        answer += is_prime(int(n))
 
+    for i in range(2, len(numbers) + 1):
+        comb = list(permutations(num_list, i))
+        for c in comb:
+            n = int(''.join(c))
+            answer += is_prime(n)
 
-
-	return answer
-
-solution("001")
+    return answer
